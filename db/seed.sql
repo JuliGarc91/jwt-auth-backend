40314c8bafa8432e7f8dc2a5fffaea1baac27cbe
juligarc91 <julissagarcia@pursuit.org>
2024-04-09 17:26:42 -0400

    added images to user plants seed

--- db/seed.sql
+++ db/seed.sql
@@ -6,6 +6,7 @@ VALUES
 ('demo', '$2b$10$.z68x3792U9LyBwmghfsKexstMO7i0SeNCoDmeJa7bEFPQBnZU3bK', 'demo@example.com', NOW(), NOW());
 
 INSERT INTO plants (userId, name, species, careInstructions, imageUrl)
-VALUES (1, 'Spider Plant', 'Chlorophytum comosum', 'Keep soil evenly moist but not waterlogged. Prefers indirect sunlight.', './images/spider-plant.webp'),
-       (1, 'Snake Plant', 'Sansevieria trifasciata', 'Allow soil to dry out between waterings. Tolerates low light conditions.', './images/snake-plant.webp'),
-       (1, 'Peace Lily', 'Spathiphyllum wallisii', 'Keep soil consistently moist. Thrives in low to medium light conditions.', './images/peace-lily.webp');
+VALUES 
+(1, 'Spider Plant', 'Chlorophytum comosum', 'Keep soil evenly moist but not waterlogged. Prefers indirect sunlight.', 'https://images.unsplash.com/photo-1611527664689-d430dd2a6774?q=80&w=1746&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+(1, 'Snake Plant', 'Sansevieria trifasciata', 'Allow soil to dry out between waterings. Tolerates low light conditions.', 'https://images.unsplash.com/photo-1599009944474-5bc0ff20db85?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+(1, 'Peace Lily', 'Spathiphyllum wallisii', 'Keep soil consistently moist. Thrives in low to medium light conditions.', 'https://images.unsplash.com/photo-1593691509543-c55fb32d8de5?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
